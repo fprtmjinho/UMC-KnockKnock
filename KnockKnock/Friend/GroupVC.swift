52b0558b2aed2357827a71a68160302ef5c200b9
timotheekim10 <timotheekim10@gmail.com>
2023-07-11 22:47:44 +0900

    찐 친 탭바 구현

---
+++ KnockKnock/Friend/GroupVC.swift
@@ -0,0 +1,24 @@
+//
+//  GroupVC.swift
+//  KnockKnock
+//
+//  Created by 티모시 킴 on 2023/07/11.
+//
+
+import UIKit
+
+class GroupVC: UIViewController {
+    
+    func makeSubView() {
+        
+    }
+    
+    func makeConstraint() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
