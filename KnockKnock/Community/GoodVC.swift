712ff9ed1daf39dbb56b679b3e87a14e4c2e7db8
timotheekim10 <timotheekim10@gmail.com>
2023-08-07 01:48:54 +0900

    fix: 게시글 작성자 연동

--- KnockKnock/Community/GoodVC.swift
+++ KnockKnock/Community/GoodVC.swift
@@ -144,7 +144,7 @@ extension GoodVC: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelega
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let postVC = PostVC()
         postVC.categoryValue = posts[indexPath.row].boardType
-        postVC.post = Post(profile: UIImage(named: "karim")!, name: "카림", title: posts[indexPath.row].title, content: posts[indexPath.row].content, imageURL: posts[indexPath.row].imageUrl, images: [], time: posts[indexPath.row].createdAt, likes: posts[indexPath.row].likes, comments: posts[indexPath.row].comments)
+        postVC.post = Post(profile: UIImage(named: "karim")!, name: posts[indexPath.row].name, title: posts[indexPath.row].title, content: posts[indexPath.row].content, imageURL: posts[indexPath.row].imageUrl, images: [], time: posts[indexPath.row].createdAt, likes: posts[indexPath.row].likes, comments: posts[indexPath.row].comments)
         postVC.hidesBottomBarWhenPushed = true
         navigationController?.pushViewController(postVC, animated: true)
     }
